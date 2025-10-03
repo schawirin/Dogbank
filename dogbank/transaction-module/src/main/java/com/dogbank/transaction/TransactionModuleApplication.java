93a2f039ba8cd346938905f0a535fdab0f3c5be1
schawirin <pedro.schawirin@datadoghq.com>
2025-10-03 20:46:56 -0300

    app security on pom.xml and config8

--- dogbank/transaction-module/src/main/java/com/dogbank/transaction/TransactionModuleApplication.java
+++ dogbank/transaction-module/src/main/java/com/dogbank/transaction/TransactionModuleApplication.java
@@ -2,8 +2,10 @@ package com.dogbank.transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = {
+@SpringBootApplication
+@ComponentScan(basePackages = {
     "com.dogbank.transaction", 
     "com.dogbank.account", 
     "com.dogbank.auth"
